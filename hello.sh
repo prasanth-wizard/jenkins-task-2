c02ed72bcede9ef12c5b454718da7e66df343175
prasanth-work <prasanth.kalichamy@outlook.com>
2025-05-17 10:27:07 +0530

    Updated for email

--- hello.sh
+++ hello.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Hello from Jenkins build
 echo "Build triggered successfully!"
-echo "This is for email notification sent correctly"
+echo "This is for email notification sent to email"
